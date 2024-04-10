81b1a07b71396e516a3c4734362175c898c52ba6
Alvin Angulu <angulualvin@gmail.com>
2024-04-10 15:32:36 +0300

    lab completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("form")
+  form.addEventListener("submit", e =>{
+    e.preventDefault();
+    const newTaskDescription = document.getElementById("new-task-description").value;
+    BuildTasks(newTaskDescription);
+    form.reset(); 
+    
+   
+  })
 });
+
+
+function BuildTasks(task){
+ // console.log(task)
+  const p = document.createElement("p")
+  p.textContent = task;
+  document.querySelector("#tasks").appendChild(p);
+  const btn = document.createElement("button");
+  btn.textContent = "DELETE"
+  p.appendChild(btn)
+  btn.addEventListener("click", deleteTask)
+}
+
+function deleteTask(e){
+  e.target.parentNode.remove();
+}
